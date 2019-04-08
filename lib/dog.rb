4a9a69d41098e804f68567d996b4099d6bd157c1
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-08 20:26:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  def initialize(name , breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name 
+    @name = name
+  end
+  
+  def breed
+    @bread=breed
+  end
+  
+  def name 
+    @name
+  end
+  
+  def breed
+    @bread
+  end
+end
